7592970b364c75106e6983a3c641b2f6bbcee3fe
Johny Mcreed <78428114+johnymcreed@users.noreply.github.com>
2022-11-09 00:53:59 -0600

    Add files via upload

--- extension/src/script.js
+++ extension/src/script.js
@@ -9,10 +9,11 @@ function fetches(link, id){fetch(link).then(response => response.text()).then(te
 // Initalizes extension
 function init ()
 {
-    console.log("Themius v1 Extension injected into echo", );
+    console.log("Themius injected into echo", );
     get(
         {
-            onOroff: true
+            onOroff: true,
+            kittens: true
         }, 
         function (object) 
         {
@@ -27,42 +28,20 @@ function init ()
 // Create Style
 function create ()
 {
-    var style = document.createElement('style');
+    let style = document.createElement('style');
     style.id = 'themius-css';
-    document.querySelector('body').appendChild(style);
+    style.type = 'text/css';
+    style.classList = "themius";
+    document.querySelector('html').appendChild(style);
 }
 
 // Add Style
 function add ()
 {
-    get(
-        {
-            themeby: [1,2,3]
-        }, 
-        function (object) 
-        {   
-            if (object.themeby == 1)
-            {
-                console.log('Theme [Auto] was injected')
-                fetches('https://raw.githubusercontent.com/johnymcreed/Themius/Default/themes/themius.v3.auto.css', 
-                        '#themius-css')
-            }
-            else if (object.themeby == 2)
-            {
-                console.log('Theme [Dark] was injected')
-                fetches('https://raw.githubusercontent.com/johnymcreed/Themius/Default/themes/themius.v3.dark.css', 
-                        '#themius-css')
-            }
-            else if (object.themeby == 3)
-            {
-                console.log('Theme [Light] was injected')
-                fetches('https://raw.githubusercontent.com/johnymcreed/Themius/Default/themes/themius.v3.light.css', 
-                        '#themius-css')
-            }  
-        });
+    fetches('https://raw.githubusercontent.com/johnymcreed/Themius/Default/themes/themius.v4.css', 
+    '#themius-css');
 }
 
-// Helps detect if echo was opened with a special attribute idenitfer
 if (document.querySelector('html').hasAttribute('buzz-version'))
 {
     init();
